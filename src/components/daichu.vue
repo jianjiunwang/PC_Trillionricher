b3fa4e9301911c915f57fcaa72df9bd76cc9308d
fe02 <819696860@qq.com>
2023-07-07 00:33:16 +0700

    1

--- src/components/daichu.vue
+++ src/components/daichu.vue
@@ -1261,11 +1261,6 @@
             },
 
             Openandclose:function () {
-                this.gettongzhi();
-                this.getexchange();
-                this.getzhifubao();
-                this.check_order()
-                return
                 $.ajax({
                     type: "POST",
                     url: "https://www.trillionricher.com/1.0/notice_switch",
